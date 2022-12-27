e6b5e70734aff8435cde41b61a76ac178796792f
wagnerp562 <115497021+wagnerp562@users.noreply.github.com>
2022-12-27 12:06:56 -0300

    projeto pokedex
    foi feito os detalhes de cada pokemon 
    projeto em construção

---
+++ assets/js/main.js
@@ -0,0 +1,48 @@
+
+const pokemonList = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById('loadMoreButton')
+const limit = 20
+let offset = 0
+
+const maxRecords = 1500
+
+
+function loadPokemonItens(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHTML = pokemons.map((pokemon) => `
+        <a  href="details.html?id=${pokemon.number}">
+            <li class="pokemon ${pokemon.type}">
+                <span class="number">#${pokemon.number}</span>
+                <span class="name">${pokemon.name}</span>
+                <div class="detail">
+                    <ol class="types">
+                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join(``)}
+                    </ol>
+                    <img src="${pokemon.photo}"
+                        alt=${pokemon.name}>
+                </div>
+            </li>
+        </a>
+    `).join('')
+        pokemonList.innerHTML += newHTML
+    })
+}
+
+loadPokemonItens(offset, limit)
+
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+
+    const qtdeRecord = offset + limit
+    if (qtdeRecord >= maxRecords) {
+        const newLimit = maxRecords - offset
+        loadPokemonItens(offset, newLimit)
+
+        loadMoreButton.parentElement.removeChild(loadMoreButton)
+
+    } else {
+        loadPokemonItens(offset, limit)
+    }
+})
+
+pokemonList.addEventListener('click',(pokemon)=>console.log(pokemon.name))
